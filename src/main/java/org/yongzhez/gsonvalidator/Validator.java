bc66ae7572c0fda1c816b9e6ac45f727a5780932
Jason Zheng <yongzhe.zheng1@gmail.com>
2016-01-08 19:21:13 -0500

    Validator(multipleOf): created and tested multipleOf keyword for number validation
    Signed-off-by: Jason Zheng <yongzhe.zheng1@gmail.com>

--- src/main/java/org/yongzhez/gsonvalidator/Validator.java
+++ src/main/java/org/yongzhez/gsonvalidator/Validator.java
@@ -1,20 +1,68 @@
 package org.yongzhez.gsonvalidator;
 
-
-
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import org.json.JSONException;
 
-import org.json.JSONObject;
 
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Iterator;
 
 /**
  * Created by youngz on 28/12/15.
  */
 public class Validator {
+
+
+
+    /**
+     * Takes in a jsonElement to be validated, and the schema that it validates
+     * @param schema
+     * @return
+     */
+    public boolean validateNumber(JsonElement jsonElement, JsonObject schema){
+
+        boolean valid = true;
+
+        //adheres to section 5.1.1 of Json schema validation for multiple of
+        if (schema.has("multipleOf")){
+            if (jsonElement.isJsonPrimitive()){
+                //if jsonElement is a primitve, check if it is a number, if it's a string, ignore it
+                JsonPrimitive element = jsonElement.getAsJsonPrimitive();
+                if (element.isNumber()){
+                    //check if either number is below 1, if so, multiply both numbers by
+                    //10 until both are above 1
+                    Double json = element.getAsDouble();
+                    Double validate = schema.get("multipleOf").getAsDouble();
+                    if ((json < 1 && json > 0)|| (validate < 1 && validate > 0)){
+                        if (json < validate){
+                            while (json < 1){
+                                json  = json*10;
+                                validate = validate*10;
+                            }
+                        }
+                        if (json > validate){
+                            while (validate < 1){
+                                json  = json*10;
+                                validate = validate*10;
+                            }
+                        }
+                    }
+                    if (json % validate != 0){
+                        valid = false;
+                    }
+                }
+            }else{
+                valid = false;
+            }
+        }
+
+        if (schema.has("maximum") || schema.has("exclusiveMaximum")){
+
+        }
+
+        return valid;
+    }
 
     /**
      * takes in a json and a schema and checks if json corresponds to schema
@@ -22,49 +70,50 @@ public class Validator {
      * @param schema a JSON representation of a schema
      * @return true if json corresponds to schema. false otherwise
      */
-    public boolean validator(JSONObject json, JSONObject schema)
+    public boolean validator(JsonElement json, JsonObject schema)
     {
-        JSONObject properties = (JSONObject)schema.get("properties");
+
         boolean valid = false;
 
-        while (properties.keys().hasNext()){
-            String key = properties.keys().next();
-            JSONObject property = (JSONObject)properties.get(key);
-
-            String type = (String)property.get("type");
-
-            switch (type) {
-                case "array":
-                    if (json.get(key) instanceof Array){
-                        valid = true;
-                    }
-                case  "boolean":
-                    if (json.get(key) instanceof Boolean){
-                        valid = true;
-                    }
-                case "integer":
-                    if (json.get(key) instanceof Integer){
-                        valid = true;
-                    }
-                case "number":
-                    if (json.get(key) instanceof Float){
-                        valid = true;
-                    }
-                case "null":
-                    if (json.get(key) == null){
-                        valid = true;
-                    }
-                case "object":
-                    // if there is a nested json, recurse through until return
-                    JSONObject nested = (JSONObject)json.get(key);
-                    boolean result = this.validator(nested, property);
-                case "string":
-                    if (json.get(key) instanceof String){
-                        valid = true;
-                    }
+        try{
+//            //get the properties field of a json schema
+//            JsonObject properties = schema.get("properties").getAsJsonObject();
+//            //iterate through all of the property fields checking for their types
+//            for (Map.Entry<String, JsonElement> entry : properties.entrySet()) {
+//
+//                String key = entry.getKey();
+//                JsonElement element = entry.getValue();
+//
+//                if(element.isJsonObject()){
+//                    JsonObject property = element.getAsJsonObject();
+//                    if (property.has("type")){
+//                        String type = property.get("type").getAsString();
+//
+//                        switch (type) {
+//                            case "object":
+//                                // if there is a nested json, recurse through until return
+//                                JsonObject nested = (JsonObject)json.get(key);
+//                                valid = this.validator(nested, property);
+//                            case "string":
+//                                if (json.get(key).getAsString() instanceof String){
+//                                    valid = true;
+//                                }
+//                        }
+//                    }
+//                }
+//
+//
+//
+//            }
+            if (schema.has("multipleOf")){
+                valid = this.validateNumber(json, schema);
             }
+
+            return valid;
+        }catch (JSONException exception){
+            return false;
         }
 
-        return valid;
+
     }
 }
---
+++ src/test/java/org/yongzhez/gsonvalidator/TestValidator.java
@@ -0,0 +1,77 @@
+package org.yongzhez.gsonvalidator;
+
+
+import com.google.gson.*;
+import junit.framework.TestCase;
+
+import org.junit.Before;
+
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by youngz on 29/12/15.
+ */
+public class TestValidator extends TestCase{
+
+
+    protected Validator validator;
+
+    @Before
+    public void setUp(){
+
+        this.validator = new Validator();
+
+    }
+
+    /**
+     * Taking in a file path, goes through every single one of the tests and runs
+     * validator against the file path.
+     * @param testJson
+     */
+    public void testHelper(String testJson){
+        try{
+            FileReader reader = new FileReader(testJson);
+            JsonParser jsonParser = new JsonParser();
+            JsonArray root = jsonParser.parse(reader).getAsJsonArray();
+
+            //go through every test/schema set
+            for (int i = 0; i < root.size(); i ++){
+                JsonObject set = root.get(i).getAsJsonObject();
+
+                JsonObject schema = set.get("schema").getAsJsonObject();
+                JsonArray tests = set.get("tests").getAsJsonArray();
+
+                for (JsonElement test: tests){
+                    //run validator on every set's test set
+                    boolean result = this.validator.validator(test.getAsJsonObject().get("data"),
+                            schema);
+                    //check against the valid field in the json file, prints out the test case corresponding
+                    assertEquals(test.getAsJsonObject().get("description").getAsString(),
+                            test.getAsJsonObject().get("valid").getAsBoolean(),
+                            result);
+
+                }
+            }
+
+        }catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }catch (JsonParseException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public void testAdditionalItems(){
+        Path path = Paths.get("multipleOf.json");
+        this.testHelper(path.toAbsolutePath().toString());
+
+    }
+}
